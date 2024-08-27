27b8ec522a441aadf2ed162407ded2dd1fa7fe22
pickles-bread-and-butter <willettisaak@gmail.com>
2024-08-27 14:03:15 +0900

    add deps

---
+++ nanapush/setup.py
@@ -0,0 +1,8 @@
+"""This file exists as a way to enable editable installs for development and usage"""
+
+#!/usr/bin/env python
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
